7d248811be51ace92c50358f313979ec0ba15647
Ylahjaily <y.lahjaily@ecole-ipssi.net>
2019-11-12 15:00:31 +0100

    Fixed stylesheets' path

--- www/View/templates/back.tpl.php
+++ www/View/templates/back.tpl.php
@@ -12,16 +12,16 @@
     <title>SB Admin - Dashboard</title>
 
     <!-- Bootstrap core CSS-->
-    <link href="public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../src/public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template-->
-    <link href="public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../src/public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <!-- Page level plugin CSS-->
-    <link href="public/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <link href="../src/public/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="public/css/sb-admin.css" rel="stylesheet">
+    <link href="../src/public/css/sb-admin.css" rel="stylesheet">
 
   </head>
 
@@ -181,23 +181,23 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="public/vendor/jquery/jquery.min.js"></script>
-    <script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../src/public/vendor/jquery/jquery.min.js"></script>
+    <script src="../src/public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="public/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../src/public/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Page level plugin JavaScript-->
-    <script src="public/vendor/chart.js/Chart.min.js"></script>
-    <script src="public/vendor/datatables/jquery.dataTables.js"></script>
-    <script src="public/vendor/datatables/dataTables.bootstrap4.js"></script>
+    <script src="../src/public/vendor/chart.js/Chart.min.js"></script>
+    <script src="../src/public/vendor/datatables/jquery.dataTables.js"></script>
+    <script src="../src/public/vendor/datatables/dataTables.bootstrap4.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="public/js/sb-admin.min.js"></script>
+    <script src="../src/public/js/sb-admin.min.js"></script>
 
     <!-- Demo scripts for this page-->
-    <script src="public/js/demo/datatables-demo.js"></script>
-    <script src="public/js/demo/chart-area-demo.js"></script>
+    <script src="../src/public/js/demo/datatables-demo.js"></script>
+    <script src="../src/public/js/demo/chart-area-demo.js"></script>
 
   </body>
 
